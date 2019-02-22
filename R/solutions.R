02d3771e39b52cd6dc629797806b68420c2a83c5
Paul <plschoenmakers@hotmail.com>
2019-02-22 11:34:02 +0100

    Update solutions.R

--- R/solutions.R
+++ R/solutions.R
@@ -1,6 +1,6 @@
 library(rjson)
 
-#Part 1
+# Part 1
 signal <- readLines("distress_signal.txt")
 sg     <- unlist(strsplit(signal,""))
 for(i in 1:length(sg)){
@@ -14,7 +14,7 @@ sg_dec         <- base64enc::base64decode(y)
 decoded_signal <- rawToChar(sg_dec)
 cat(decoded_signal)
 
-#Part 2
+# Part 2
 ppb_txt <- readLines("ppb.bin.log.txt")
 ppb     <- unlist(strsplit(ppb_txt," "))
 bit_v   <- 2^(7:0)
@@ -45,7 +45,7 @@ id_sep <- sapply(seq(1, nchar(id), by=2), function(x) substr(id, x, x+1))
 place  <- rawToChar(as.raw(strtoi(id_sep, 16L)))
 cat(place)
 
-#Part 3
+# Part 3
 flood <- readLines("flood.txt")
 fld   <- fromJSON(flood)
 for(j in 1:length(fld$regions)){
